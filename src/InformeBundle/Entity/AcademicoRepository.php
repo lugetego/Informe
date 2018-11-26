f5fdd95aa43e178d4c4186326b68709a5c20bc59
lugetego <lugetego@hotmail.com>
2018-11-26 11:51:37 -0600

    orden ASC academico admin

--- src/InformeBundle/Entity/AcademicoRepository.php
+++ src/InformeBundle/Entity/AcademicoRepository.php
@@ -12,5 +12,16 @@ use Doctrine\ORM\EntityRepository;
 class AcademicoRepository extends EntityRepository
 {
 
+    /**
+     * Change default sort order for entities
+     * Sorted by name ascending.
+     *
+     * @return array of entities:
+     */
+    public function findAll()
+    {
+        return $this->findBy(array(), array('apellido' => 'ASC'));
+    }
+
 
 }
--- src/InformeBundle/Repository/AcademicoRepository.php
+++ src/InformeBundle/Repository/AcademicoRepository.php
@@ -25,3 +25,6 @@ class AcademicoRepository extends EntityRepository
 
 
 }
+
+
+
